455e2b7161c60fa234db8267a6797fcd414c4a23
Feranmi Dere <dereferanmi@gmail.com>
2025-09-24 10:27:18 +0100

    Added hyperparameter tuning
    Created XGBoost and Random Forest models, and used a randomised search with cross-validation to tune the hyperparameters

---
+++ src/hyperparameter_tuning.py
@@ -0,0 +1,51 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import *
+import xgboost as xgb
+import pandas as pd
+from scipy.stats import randint, uniform
+from sklearn.model_selection import RandomizedSearchCV, train_test_split
+import joblib as jb
+
+data = pd.read_csv('../data/train_resampled.csv')
+X, y = data.drop(columns=['Class']), data['Class']
+X_sample, _, y_sample, _ = train_test_split(
+    X, y, train_size=0.2, stratify=y, random_state=42
+)
+
+models = {
+    'RandomForest': RandomForestClassifier(random_state=42),
+    'XGBoost': xgb.XGBClassifier(eval_metric='logloss', random_state=42)
+}
+
+param_distributions = {
+    'RandomForest': {
+        'n_estimators': randint(50, 200),
+        'max_depth': randint(3, 20),
+        'min_samples_split': randint(2, 10),
+        'min_samples_leaf': randint(1, 10),
+        'max_features': ['sqrt', 'log2']
+    },
+    'XGBoost': {
+        'n_estimators': randint(50, 200),
+        'max_depth': randint(3, 20),
+        'learning_rate': uniform(0.01, 0.3),
+        'subsample': uniform(0.5, 0.5),
+        'colsample_bytree': uniform(0.5, 0.5)
+    }
+}
+
+for name, model in models.items():
+    random_search = RandomizedSearchCV(
+        model,
+        param_distributions[name],
+        n_iter=50,
+        scoring='f1',
+        cv=5,
+        n_jobs=-1,
+        random_state=42,
+    )
+    random_search.fit(X_sample, y_sample)
+    best_model = random_search.best_estimator_
+    models[name] = best_model
+    print(f"Best parameters for {name}: {random_search.best_params_}")
+    jb.dump(best_model, f'../models/{name}_best_model.joblib')
