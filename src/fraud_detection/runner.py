788ab3574e3f84f5f8e7b1155fd44d18611752e5
Feranmi Dere <dereferanmi@gmail.com>
2026-07-21 23:32:30 +0100

    Delete runner.py

--- src/fraud_detection/runner.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-files = ["data_script.py", "imbalance_handling.py", "hyperparameter_tuning.py", "model.py"]
-
-for f in files:
-    print(f"Running {f}...")
-    subprocess.run(["python", f], check=True)
